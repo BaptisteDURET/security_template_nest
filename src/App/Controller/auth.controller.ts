1515398fe6038ca205b4024611b94ff65b826f0e
Baptiste DURET <baptiste.duret@altays.com>
2025-08-08 17:59:35 +0200

    prettier fix

--- src/App/Controller/auth.controller.ts
+++ src/App/Controller/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, UseInterceptors } from "@nestjs/common";
-import { AnyFilesInterceptor } from "@nestjs/platform-express";
-import { AuthenticationDto } from "src/Domain/Business/Authentication/auth.dto";
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { AuthenticationDto } from 'src/Domain/Business/Authentication/auth.dto';
 
 @Controller('auth')
 export class AuthenticationController {
